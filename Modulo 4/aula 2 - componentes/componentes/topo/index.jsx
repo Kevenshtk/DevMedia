67388adb684fa16acd7e8495ef3b7b9b736333d0
Keven Di Camargo Elpidio <102769882+Kevenshtk@users.noreply.github.com>
2024-07-10 21:54:24 -0300

    create modulo 4 aula 2
    aula sobre criação de componentes em React

---
+++ Modulo 4/aula 2 - componentes/componentes/topo/index.jsx
@@ -0,0 +1,9 @@
+import estilos from "./Topo.module.css"
+
+export default function Topo(){
+    return(
+        <header className={estilos.topo}>
+            <h1>Trabalhando com componetes</h1>
+        </header>
+    )
+}
