c193712b6e494cc1c07aca7e4e96e37b95028d06
Keven Di Camargo Elpidio <102769882+Kevenshtk@users.noreply.github.com>
2024-07-10 21:46:11 -0300

    commit correcao

--- Modulo 4/minha-pagina/minha-pagina/src/componentes/rodape/index.jsx
+++
@@ -1,9 +0,0 @@
-import estilos from "./rodape.module.css"
-
-export default function Rodape(){
-    return(
-        <footer className={estilos.rodape}>
-            <h2>Desenvolvido com React</h2>
-        </footer>
-    )
-}
--- Modulo 4/minha-pagina/minha-pagina/src/componentes/topo/index.jsx
+++
@@ -1,9 +0,0 @@
-import estilos from "./Topo.module.css"
-
-export default function Topo(){
-    return(
-        <header className={estilos.topo}>
-            <h1>Trabalhando com componetes</h1>
-        </header>
-    )
-}
