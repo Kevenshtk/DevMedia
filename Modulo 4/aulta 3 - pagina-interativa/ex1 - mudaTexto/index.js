bee094dbe219a9c0e5ba1cc923ff4dffca32f9a6
Keven Di Camargo Elpidio <102769882+Kevenshtk@users.noreply.github.com>
2024-08-01 14:41:07 -0300

    create modulo 4 aula 3
    aula sobre a criação de páginas interativas, exemplo 1 e 2

---
+++ Modulo 4/aulta 3 - pagina-interativa/ex1 - mudaTexto/index.js
@@ -0,0 +1,17 @@
+'use client';
+import { useState } from 'react';
+
+export default function Home() {
+  const [mensagem, setMensagem] = useState('O React é uma biblioteca do JavaScript.');
+
+  function alterarMensagem(){
+    setMensagem("O React é um framework utilizado em aplicações web.");
+  }
+
+  return (
+    <div>
+    <p>{mensagem}</p>
+    <button onClick={alterarMensagem}>Clique aqui</button>
+  </div>
+  );
+}
