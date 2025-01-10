31a13a6ca9a825b457256a77ec1854a2da3a4987
keven-shtk <kevendicamargo@gmail.com>
2025-01-10 15:10:17 -0300

    projeto restaurant

---
+++ Modulo 4/projetos/restaurant/src/app/layout.js
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Restaurant",
+  description: "",
+  robots: "index, follow",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
